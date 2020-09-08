5ed5e34601bebdbfe4844b5db6c685c84b84ea6c
Kaian Ferreira dos santos <kaianfsantos@gmail.com>
2020-09-08 03:56:58 -0300

    frontend configuration

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    {{teste}}
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      teste: null,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
